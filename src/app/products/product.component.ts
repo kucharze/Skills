c49aca3741b24aa5effe093a3b43e1c6dd2b1a0a
Zachary Kuchar <zacharyk323@live.com>
2024-01-17 16:04:35 -0500

    Used ngFor to display items in an array

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-product',
-    template: ''
+    template: `<li *ngFor="let dept of departments">
+            {{dept}}</li>`
 })
 export class ProductComponent{
     departments = [
