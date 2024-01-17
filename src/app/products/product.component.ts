268f324fe6f6bb8c1db48cad95941bb8f742b7f9
Zachary Kuchar <zacharyk323@live.com>
2024-01-17 15:31:09 -0500

    Added new component

---
+++ src/app/products/product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-product',
+    template: ''
+})
+export class ProductComponent{
+    departments = [
+        'Mountain Bicylces','Road bikes','Hybrid bikes','Bike pumps'
+    ]
+}
